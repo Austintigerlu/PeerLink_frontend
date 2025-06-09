b0781ff09d562fef8bd3b658f6916705cf86a37a
austintigerlu <austintigerlu@gmail.com>
2025-06-09 02:25:24 -0400

    start of skeleton

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text> SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerClassName='h-full'>
+        <View className='flex-1 items-center justify-center'>
+          <Text className='text-2xl font-bold'>Sign In</Text>
+          <Text className='mt-4 text-gray-600'>Please enter your credentials to sign in.</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
